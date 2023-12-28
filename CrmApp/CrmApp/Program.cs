2463fe2a4bcc358218702e227c98faa3b7278205
HalitCanbaz <107313207+HalitCanbaz@users.noreply.github.com>
2023-12-28 15:53:04 +0300

    f

---
+++ CrmApp/CrmApp/Program.cs
@@ -0,0 +1,54 @@
+using CrmApp.Extensions;
+using CrmApp.Models;
+using CrmApp.Models.OptionsModels;
+using CrmApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CrmAppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddIdentityWithExtensions();
+builder.Services.AddScoped<IEmailServices, EmailServices>();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    var cookieBuilder = new CookieBuilder();
+    cookieBuilder.Name = "AppCookie";
+    options.LoginPath = new PathString("/User/SignIn");
+    options.Cookie = cookieBuilder;
+    options.ExpireTimeSpan = TimeSpan.FromDays(60);
+    options.SlidingExpiration = true;
+
+});
+builder.Services.AddDistributedMemoryCache();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=User}/{action=SignIn}/{id?}");
+
+app.Run();
